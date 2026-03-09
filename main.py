84cefad6726b562e439ff5ff09864e80d6972b6a
James Bakis <s3783333@student.rmit.edu.au>
2026-03-09 11:48:58 +1100

    key listen set up

--- main.py
+++ main.py
@@ -1,19 +1,41 @@
 import tkinter as tk
 from square import square
-# from pynput.keyboard import Key, Listener
-import keyboard
 
 cells = []
 player_pos = [3, 3]
+
+def move_right(direction):
+    player_row = player_pos[0]
+    player_col = player_pos[1]    
+    cells[player_row][player_col].set_empty()
+    print("right")
+
+def move_left(direction):
+    player_row = player_pos[0]
+    player_col = player_pos[1]    
+    cells[player_row][player_col].set_empty()
+    print("left")
+
+def move_up(direction):
+    player_row = player_pos[0]
+    player_col = player_pos[1]    
+    cells[player_row][player_col].set_empty()
+    print("up")
+
+def move_down(direction):
+    player_row = player_pos[0]
+    player_col = player_pos[1]    
+    cells[player_row][player_col].set_empty()
+    print("down")
 
 def main():
     print("hello")
     root = tk.Tk()
     root.title("Collect Crystals")
     root.configure(background="red")
+    root.geometry("700x700+300+0")
     # root.minsize(720, 480)
     # root.maxsize(720, 480)
-    root.geometry("700x700+300+0")
     
     # label1 = tk.Label(root, text="Collect crystals!")
     # label2 = tk.Label(root, text="Avoid enemies!")
@@ -31,19 +53,12 @@ def main():
             else:
                 cell.set_empty()
             cells[row].append(cell)
-    root.bind("<Right>", movement)
+    root.bind("<Right>", move_right)
+    root.bind("<Left>", move_left)
+    root.bind("<Up>", move_up)
+    root.bind("<Down>", move_down)
 
     root.mainloop()
-
-def movement(direction):
-    player_row = player_pos[0]
-    player_col = player_pos[1]
-    if direction.keysym == "Right":
-        print(player_row)
-        print(player_col)
-        cells[player_row][player_col].set_empty()
-        print("Right")
-
 
 if __name__ == "__main__":
     main()
